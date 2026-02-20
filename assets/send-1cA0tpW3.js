9de266f79f1ccb7bede580abba5e49a9ced74e57
Andres-L1 <andreslgumuzio@gmail.com>
2026-02-20 12:18:18 +0100

    Updates

---
+++ assets/send-1cA0tpW3.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-DcnL8Cmp.js";/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=e("Send",[["path",{d:"m22 2-7 20-4-9-9-4Z",key:"1q3vgg"}],["path",{d:"M22 2 11 13",key:"nzbqef"}]]);export{n as C,o as S};
