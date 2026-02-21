2d3d4329de0f60bd9d194df21eed1995c79fed21
Andres-L1 <andreslgumuzio@gmail.com>
2026-02-21 11:12:37 +0000

    Deploying to gh-pages from @ Andres-L1/TopChess@1bec25f490b1af209dd03dbcb63e6d7ecb6dd14c 🚀

--- assets/Skeleton-Dd1Il-RC.js
+++
@@ -1,6 +0,0 @@
-import{c as o,j as r}from"./index-Dwoje0VI.js";/**
- * @license lucide-react v0.378.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=o("Trophy",[["path",{d:"M6 9H4.5a2.5 2.5 0 0 1 0-5H6",key:"17hqa7"}],["path",{d:"M18 9h1.5a2.5 2.5 0 0 0 0-5H18",key:"lmptdp"}],["path",{d:"M4 22h16",key:"57wxv0"}],["path",{d:"M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22",key:"1nw9bq"}],["path",{d:"M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22",key:"1np0yb"}],["path",{d:"M18 2H6v7a6 6 0 0 0 12 0V2Z",key:"u46fv3"}]]),c=({className:a="",variant:t="text",width:s,height:d})=>{const p="animate-pulse bg-white/5 rounded-md";let e="";return t==="circular"&&(e="rounded-full"),t==="text"&&(e="h-4 w-3/4 rounded"),r.jsx("div",{className:`${p} ${e} ${a}`,style:{width:s,height:d}})};export{c as S,n as T};
