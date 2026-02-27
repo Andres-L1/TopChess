ec5fab896adfdb8222f75234d8ae4ef0d42fe1a6
Andres-L1 <andreslgumuzio@gmail.com>
2026-02-27 01:09:41 +0100

    Updates

---
+++ assets/loader-circle-Dwhif_kE.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-HQ6nHTsK.js";/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=a("Activity",[["path",{d:"M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2",key:"169zse"}]]);/**
+ * @license lucide-react v0.378.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{t as A,c as L};
